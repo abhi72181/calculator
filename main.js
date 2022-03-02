7a8d998b43e738075ec5c6c245f7acbdf2fc484f
abhi72181 <abhihits0404@gmail.com>
2022-03-03 01:35:25 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const add=require("./add")
+const divide=require("./divide")
+const multiply=require("./multiply")
+const subtract=require("./subtract")
+
+console.log(`addition is ${add(10,2)}`);
+console.log(`division is ${divide(10,2)}`);
+console.log(`multiplication is ${multiply(10,2)}`);
+console.log(`subtraction is ${subtract(10,2)}`);
