d0c65004991ce237e95b8c35f35fe338c0e29bab
abhi72181 <abhihits0404@gmail.com>
2022-03-03 21:39:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,4 +6,4 @@ const subtract=require("./subtract")
 console.log(`addition is ${add(10,2)}`);
 console.log(`division is ${divide(10,2)}`);
 console.log(`multiplication is ${multiply(10,2)}`);
-console.log(`subtraction is ${subtract(10,2)}`);
+console.log(`subtraction is ${subtract(10,2)}`)
